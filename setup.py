92bac28a5966368052d26a44b945d70751619fe8
Tim Harder <radhermit@gmail.com>
2015-03-28 18:01:21 -0400

    setup.py: add pytest testsuite support

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import os
+import subprocess
+import sys
+
+from setuptools import setup, Command
+
+
+class RunCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+
+class PyTest(RunCommand):
+    user_options = [('match=', 'k', 'Run only tests that match the provided expressions')]
+
+    def initialize_options(self):
+        self.match = None
+
+    def run(self):
+        cli_options = ['-k', self.match] if self.match else []
+        os.environ['EPYTHON'] = 'python{}.{}'.format(sys.version_info.major, sys.version_info.minor)
+        errno = subprocess.call(['py.test'] + cli_options)
+        raise SystemExit(errno)
 
 
 # workaround to get version without importing anything
@@ -21,6 +48,8 @@ setup(
     license='MIT',
     packages=['vimball'],
     entry_points={'console_scripts': ['vimball = vimball:main']},
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     platforms='Posix',
     classifiers=(
         'Intended Audience :: Developers',
